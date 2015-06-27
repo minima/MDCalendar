10959ac994a2962b441c81332df5f4f515c04609
Daniel Yoo <misteryoo@gmail.com>
2015-06-27 19:07:09 -0400

    Add missing UIKit import to make it usable via swift.

--- MDCalendar/MDCalendar.h
+++ MDCalendar/MDCalendar.h
@@ -22,7 +22,9 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-#import <Foundation/Foundation.h>
+@import Foundation;
+@import UIKit;
+
 #import "NSDate+MDCalendar.h"
 
 /**
